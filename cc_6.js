0a6d04dfaf6338879bd65facf3cab0bbdfe9e0c3
Ohm Patel <ohmp3@usf.edu>
2026-02-27 19:29:16 -0500

    Created Sample Employees and Managers

--- cc_6.js
+++ cc_6.js
@@ -23,3 +23,21 @@ class Manager extends Employee {
         return `Manager: ${this.name}, Department: ${this.department}, Team Size: ${this.teamSize}`
     }
 }
+
+// Step 4: Creating Employees and Managers
+
+const employee1 = new Employee("JimJim", "Finance")
+const employee2 = new Employee("TimTim", "HR")
+const employee3 = new Employee("BimBim", "Marketing")
+const employee4 = new Employee("JimJim", "Engineering")
+
+const manager1 = new Manager("TheBigMan", "Logistics", 10)
+const manager2 = new Manager("TheLargeMan", "Product Design", 5)
+
+console.log(employee1.describe())
+console.log(employee2.describe())
+console.log(employee3.describe())
+console.log(employee4.describe())
+console.log(manager1.describe())
+console.log(manager2.describe())
+
