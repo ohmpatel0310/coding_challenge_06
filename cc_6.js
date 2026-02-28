1118fe0448acc502b2e20e9e7ea6d0c3d687e0a4
Ohm Patel <ohmp3@usf.edu>
2026-02-27 19:09:58 -0500

    Created Subclass Called Manager That Extends Employee

--- cc_6.js
+++ cc_6.js
@@ -1,9 +1,25 @@
+// Step 2: Create Base Class Employee
+
 class Employee {
     constructor (name, department){
         this.name = name
         this.department = department
     }
     describe(){
-        return `Name: ${this.name}, Department: ${this.department}`
+        return `Employee Name: ${this.name}, Department: ${this.department}`
+    }
+}
+
+// Step 3: Creating Manager Subclass
+
+class Manager extends Employee {
+    constructor(name, department, teamSize) {
+        super(name, department)
+        this.teamSize = teamSize
+
+    }
+
+    describe() {
+        return `Manager: ${this.name}, Department: ${this.department}, Team Size: ${this.teamSize}`
     }
 }
